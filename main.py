1613a7da8a44ca1277b05637bead5ac57b129a2a
Isidr0 <rtwatson86@gmail.com>
2021-11-16 16:29:47 -0500

    turtle commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-from turtle import Turtle
+# # importing all with * isn't recommended
+# from turtle import *
+# # imports the turtle module, but you have to define where classes are coming from with
+# # tim = turtle.Turtle()
+# import turtle
+# # import module as a shorter alias so it's easier to reference.
+# import turtle as t
+# from the turtle module, import the classes Turtle and Screen
+from turtle import Turtle, Screen
+
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("deep pink")
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+screen = Screen()
+screen.exitonclick()
+
+import heroes
+print(heroes.gen())
